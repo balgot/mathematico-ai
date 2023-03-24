158557242ee9be2ed54a3e8d72d4de50d66e7859
michal <michal.barnisin14@gmail.com>
2023-03-24 12:50:37 +0100

    Restrure project folder #1 (WIP)

--- mathematico/game/mathematico/__init__.py
+++
@@ -1,8 +0,0 @@
-from .game import Arena, Board, Mathematico, Player
-from .players import HumanPlayer, RandomPlayer, SimulationPlayer, MctsPlayer
-
-
-__all__ = [
-    "Arena", "Board", "Mathematico", "Player",
-    "HumanPlayer", "RandomPlayer", "SimulationPlayer", "MctsPlayer"
-]
--- mathematico/game/mathematico/players/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._human_player import HumanPlayer
-from ._random_player import RandomPlayer
-from ._random_simulations import SimulationPlayer
-from ._mcts_player._mcts_player import MctsPlayer
-
-
-
-__all__ = ["RandomPlayer", "HumanPlayer", "SimulationPlayer", "MctsPlayer"]
