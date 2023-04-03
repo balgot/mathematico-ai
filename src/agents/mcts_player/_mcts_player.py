7fa3b4111b2d55e6a2022acbaaa15bc1587a317a
Michal Barnišin <michal.barnisin14@gmail.com>
2023-04-03 23:33:44 +0200

    [fix] Add current mcts agent.

--- src/agents/mcts_player/_mcts_player.py
+++ src/agents/mcts_player/_mcts_player.py
@@ -1,24 +1,38 @@
-from libs.mathematico.game import Player, Board
-from ._mcts_state import MathematicoState
-from libs.MCTS.mcts import mcts
+import os, sys
+sys.path.append(os.path.join("../../"))
+
 import math
+
+from mathematico import Board, Player
+from src.utils.mcts import MCTS
+from ._mcts_state import Action, MathematicoState
+
+
+_EXPLORATION = 1 / math.sqrt(2)
+
 
 class MctsPlayer(Player):
     """
-    Human player takes inputs from console after printing the
-    board and the next move number
+    MCTS player for the Mathematico game.
+
+    Each time a decision about the next move is made, runs the MCTS
+    with given resources contraints and finds the optimal move.
+
+    Note: uses random policy for rollouts
     """
 
-    def __init__(self, timeLimit=10, explorationConstant=1 / math.sqrt(2)):
+    def __init__(self,
+        max_time_ms: int = 100,
+        max_simulations: 'int | None' = None,
+        exploration_constant: float = _EXPLORATION
+    ):
         super().__init__()
-        self.mcts = mcts(timeLimit=timeLimit, explorationConstant=explorationConstant)
-    
+        self.mcts = MCTS(max_time_ms, max_simulations, exploration_constant)
+
     def reset(self) -> None:
         self.board = Board()
 
     def move(self, number: int):
         state = MathematicoState(self.board, number)
-        result = self.mcts.search(initialState=state, needDetails=True)
-        action = result["action"]
-        
+        action: Action = self.mcts.search(state)[0]
         self.board.make_move(action, number)
--- src/agents/mcts_player/_mcts_state.py
+++ src/agents/mcts_player/_mcts_state.py
@@ -1,46 +1,51 @@
-from typing import Tuple, Iterator, List
-from copy import deepcopy
-from libs.mathematico import Board
-from random import randrange, random
-from numpy import concatenate
-Action = Tuple[int, int]
+import os, sys
+sys.path.append(os.path.join("../../"))
 
-class MathematicoState():
-    def __init__(self, board, number):
+from copy import deepcopy
+from random import randrange
+
+from mathematico import Board
+from src.utils.mcts import StateI
+
+
+# No need to implement Action object. An action object needs to implement
+# __hash__ and __eq__, which tuple does.
+Action = tuple[int, int]
+"""Position on the board."""
+
+
+class MathematicoState(StateI):
+    def __init__(self, board: Board, card: int):
         self.board: Board = board
-        self.number: int = number
-        self.numbers_left: List[int] = [i for i in range(1, 14) for _ in range(4)]
-        self.numbers_left.remove(number)
-    
-    def getCurrentPlayer(self):
-        # 1 for maximiser, -1 for minimiser
+        self.card: int = card
+        deck = {i: 4 for i in range(1, 13+1)}
+        for i in range(self.board.size):
+            for j in range(self.board.size):
+                current_card = self.board.grid[i][j]
+                if current_card != 0:
+                    deck[current_card] -= 1
+        deck[card] -= 1
+        self.numbers_left = [card
+                    for card, count in deck.items()
+                    for _  in range(count)]
+
+    def get_current_player(self) -> int:
         return 1
 
-    def getPossibleActions(self) -> Iterator[Action]:
-        return self.board.possible_moves()
+    def get_possible_actions(self) -> list[Action]:
+        return list(self.board.possible_moves())
 
-    def takeAction(self, action: Action):
-        newState = deepcopy(self)
-        newState.board.make_move(action, newState.number)
-        # pick a random card as next 
-        newState.number = newState.numbers_left.pop(randrange(len(newState.numbers_left)))
-        return newState
-    
-    def isTerminal(self):
+    def take_action(self, action: Action) -> 'MathematicoState':
+        next_state = deepcopy(self)
+        next_state.board.make_move(action, next_state.card)
+        # pick a random card as next
+        card_idx = randrange(len(next_state.numbers_left))
+        next_state.card = next_state.numbers_left.pop(card_idx)
+        return next_state
+
+    def is_terminal(self) -> bool:
         # State is terminal if all numbers are placed on board
         return self.board.occupied_cells == self.board.size ** 2
 
-    def getReward(self):
+    def get_reward(self) -> int:
         return self.board.score()
-            
-
-    def __eq__(self, other):
-        return self.__class__ == other.__class__ and \
-            self.board.grid == other.board.grid and \
-            self.number == other.number
-
-
-    def toVector(self):
-        return concatenate(self.board.grid) + [self.number]
-# No need to implement Action object. An action object needs to implement
-# __hash__ and __eq__, which tuple does.
